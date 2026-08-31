a1cb924eeffb9f7c180c816d44ef079a463d1674
NitinRKulkarni <nitin.r.kulkarni2005@gmail.com>
2026-08-31 23:07:05 +0530

    Remove java directory from Git tracking

--- java/src/main/java/com/omnicfo/repository/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.omnicfo.repository;
-
-import com.omnicfo.model.entity.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrganizationRepository extends JpaRepository<Organization, UUID> {
-}
